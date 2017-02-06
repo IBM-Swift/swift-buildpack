a1ac1fe643597dc1937b8ffdeec30da239093965
Christian Compton <cbcompto@us.ibm.com>
2017-02-06 13:40:37 -0600

    Refactoring SSH tests main script

--- ci/execute_script.sh
+++ ci/execute_script.sh
@@ -28,7 +28,7 @@ SWIFT_HELLOWORLD_REPUSH_TIME=$5
 ./ci/performance_validator.sh Kitura-Starter $KITURA_STARTER_PUSH_TIME $TIMES_TO_REPEAT_TO_SUCCESS $KITURA_STARTER_REPUSH_TIME
 rm -rf Kitura-Starter
 ./ci/performance_validator.sh swift-helloworld $SWIFT_HELLOWORLD_PUSH_TIME $TIMES_TO_REPEAT_TO_SUCCESS $SWIFT_HELLOWORLD_REPUSH_TIME
-cp credentials-buildpack-test/.ssh/* swift-helloworld/.ssh
+cp -R credentials-buildpack-test/.ssh swift-helloworld/.ssh
 chmod 600 swift-helloworld/.ssh/swiftdevops_test_rsa
 sed -i 's/^ *dependencies:.*/dependencies: [\.Package(url: "git@github.ibm.com\:IBM-Swift\/credentials-buildpack-test.git", majorVersion: 1, minor: 0)]' swift-helloworld/Package.swift
 ./ci/performance_validator.sh swift-helloworld $SWIFT_HELLOWORLD_PUSH_TIME $TIMES_TO_REPEAT_TO_SUCCESS $SWIFT_HELLOWORLD_REPUSH_TIME
